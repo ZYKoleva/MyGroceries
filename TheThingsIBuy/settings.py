68a586afd9c3583e1616c077faa512e8f97a1c50
ZYKoleva <zykoleva@gmail.com>
2021-01-17 11:22:39 +0200

    Added allowed hosts

--- TheThingsIBuy/settings.py
+++ TheThingsIBuy/settings.py
@@ -29,7 +29,11 @@ SECRET_KEY = os.environ.get('MY_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    'http://127.0.0.1',
+    'http://myprodstat.herokuapp.com'
+]
 
 
 # Application definition
