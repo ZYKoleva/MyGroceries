488d606f1f0a03241fa17ef02dce73311051ab33
ZYKoleva <zykoleva@gmail.com>
2021-01-18 11:28:04 +0200

    updated settings

--- TheThingsIBuy/settings.py
+++ TheThingsIBuy/settings.py
@@ -98,14 +98,7 @@ DATABASES = {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': 'my_app_db',
-    #     'USER': 'postgres',
-    #     'PASSWORD': 'password',
-    #     'HOST': '127.0.0.1',
-    #     'PORT': '5432',
-    # }
+
 }
 
 
@@ -144,10 +137,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
-STATIC_URL = '/static/'
 STATIC_ROOT = join(BASE_DIR, 'staticfiles')
-
+STATIC_URL = '/static/'
 STATICFILES_DIRS = (join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
